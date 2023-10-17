d353ab78383b2124e51afa975c6b2fb82ea10a35
Alexey Khristolubov <alex-khrist98@mail.ru>
2023-10-17 12:07:42 +0500

    core: добавляет слой доменов, модели ОРМ и сериализаторы

---
+++ domains/models.py
@@ -0,0 +1,17 @@
+"""В этом модуле хранятся модели для работы с БД при помощи SQLAlchemy.
+Модели валидации и сериализации (pydantic) хранятся в serializers.py
+"""
+
+from sqlalchemy import Column, Integer, Text, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Question(Base):
+    """Этот класс представляет собой модель, отражающую таблицу"""
+    __tablename__ = "questions"
+    id = Column(Integer, primary_key=True, index=True)
+    question_text = Column(Text, index=True, unique=True)
+    answer_text = Column(Text)
+    creation_date = Column(DateTime)
+    
---
+++ domains/serializers.py
@@ -0,0 +1,20 @@
+"""Этот модуль содержит модели для сериализации и валидации данных
+"""
+from datetime import datetime
+from pydantic import BaseModel, Field 
+
+class QuestionsPost(BaseModel): 
+    """Этот класс содержит модель для валидации тела ПОСТ запроса на эндпоинт questions
+    """
+    questions_num:int = Field(ge=1)
+
+class Question(BaseModel):
+    """Этот класс содержит в себе сериализатор для данных из БД. 
+    Схема данных в бд находится в файле models.py
+    """
+    id:int
+    question_text: str
+    answer_text:str
+    creation_date:datetime
+    
+    
